340724fcf1c70283d4f814ca310c758a85e235d8
dipendradangol <dipendra.dangol11@gmail.com>
2020-06-21 18:14:31 +0300

    fetch meme img, displaying templates & update the state

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+           <header>
+               <h1 style={{ textAlign: "center"}}>Pick a template</h1>
+           </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Meme from "./Meme";
+
+const MemeGenerator = () => {
+    const [templates, setTemplates] = useState([]);
+    const [template, setTemplate] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(x => x.json().then(response => setTemplates(response.data.memes))
+        )}, []);
+    return (
+        <div style={{ textAlign: "center" }}>
+            {template && <Meme template={template} />}
+            {!template && 
+                templates.map(template => {
+                    return(
+                        <Meme 
+                            template={template}
+                            onClick={() =>{
+                                setTemplate(template)
+                            }}
+                         />
+                    )
+                })}
+        </div>
+    );
+}
+
+export default MemeGenerator;
